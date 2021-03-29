2c8a4da2e2d5fa195c098def75e9e99d646ce528
konriz <konriz@gmail.com>
2021-03-29 15:38:47 +0200

    CORS enabled

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const whitelist = ["http://localhost:8080"];
+  const originFunction = (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      // TODO manage CORS properly!
+      callback(null, true);
+    }
+  };
+  const corsOptions = {
+    credentials: true, origin: originFunction
+  };
+  app.enableCors(corsOptions);
+  app.use(helmet({hsts: false}));
+
   await app.listen(3000);
 }
 
